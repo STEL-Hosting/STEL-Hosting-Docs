dd07d94ee5946688c4c4fc99a0e7feffba1aee30
STEL Hosting <158850593+STEL-Hosting@users.noreply.github.com>
2024-02-07 00:45:07 -0600

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -45,7 +45,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/STEL-Hosting/STEL-Hosting-Docs/',
+            'https://github.com/STEL-Hosting/STEL-Hosting-Docs/blob/',
         },
         blog: false,
         theme: {
